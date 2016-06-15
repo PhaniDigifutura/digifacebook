786bbec70e4bb4fafd0e8c8692d66f33b746d7f2
Phani Ramakrishna <phani@digifutura.com>
2016-06-15 15:31:19 +0530

    Changed app id and secret to environment variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "485346685003603", "e537d18451cbf0fba340f78b101e928a"
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]  
 end
